df455cba5b21c947c9c549f14f3f9eaccbda6407
raul tamayo briones <raultamayobriones@gmail.com>
2026-09-11 17:52:13 -0600

    inicializaion del proyecto

---
+++ src/main/java/com/tvmaze/api/TvmazeApiApplication.java
@@ -0,0 +1,13 @@
+package com.tvmaze.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmazeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvmaze/api/TvmazeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvmaze.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
